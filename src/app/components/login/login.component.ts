624f22af91e91cd39cc68aef5e975305f5fe9f33
mschader <mail@markusschader.de>
2017-12-07 16:17:28 +0100

    Add login component

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  test: string = 'just a test';
+  constructor(private auth: AuthService) { }
+
+  ngOnInit(): void {
+    console.log(this.auth.test());
+  }
+
+}
