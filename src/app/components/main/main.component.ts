0dc679f4d989dcc9308cb85c7675b54c1b49ba85
mschader <mail@markusschader.de>
2017-12-19 16:09:51 +0100

    Move code morea neat together

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -28,37 +28,25 @@ export class MainComponent implements OnInit {
   }
 
   getGroups(): void {
-    this.api.getGroups()
-    .then((result) => {
-      console.log(result.json())
+    this.api.getGroups().then((result) => {
       this.groups = result.json()
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+    }).catch((err) => { console.log(err) })
   }
 
   addGroup(): void {
-    this.api.addGroup(this.newGroup)
-    .then((result) => {
-      console.log(result.json())
+    this.api.addGroup(this.newGroup).then((result) => {
       this.status = 'Group added'
       this.getGroups()
-    })
-    .catch((err) => {
+    }).catch((err) => {
       console.log(err)
       this.status = err
     });
   }
 
   joinGroup(groupName): void {
-    this.api.joinGroup(groupName)
-    .then((result) => {
+    this.api.joinGroup(groupName).then((result) => {
       this.updateUserInfo()
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+    }).catch((err) => { console.log(err) })
   }
 
   ngOnInit(): void {
