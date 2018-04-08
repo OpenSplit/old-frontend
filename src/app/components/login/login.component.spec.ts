586dfe8488357b6da0eda3f377b3beaaf5245eb4
megfault <ana@megfau.lt>
2018-04-08 18:05:55 +0200

    test: update login component tests

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,6 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
+import { AuthService } from '../../services/auth.service';
 import { LoginComponent } from './login.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs/observable/of';
+import { RouterTestingModule } from '@angular/router/testing';
+
+class mockAuthService {
+  login = jasmine.createSpy('login');
+  tokenLogin = jasmine.createSpy('tokenLogin');
+}
+
+const mockToken = "mockToken";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,9 +20,25 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [LoginComponent],
+      imports: [
+        FormsModule,
+        RouterTestingModule
+      ],
+      providers: [
+        {
+          provide: AuthService,
+          useClass: mockAuthService
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of(mockToken)
+          }
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
