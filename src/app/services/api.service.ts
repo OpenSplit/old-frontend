d8ad29bdcbb2c4cc34471b367d70a7e8023d2914
Thomas Schulze <tschulze@codemonauts.com>
2018-05-13 14:06:40 +0200

    fix some api issues

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -16,32 +16,38 @@ export class ApiService {
   constructor(private http: Http) { }
 
   getUserInfo(): Promise<any> {
-    let url: string = `${this.BASE_URL}/user`;
+    let url: string = `${this.BASE_URL}/users`;
     return this.http.get(url, {headers: this.headers}).toPromise();
   }
 
   getGroupInfo(id): Promise<any> {
-    let url: string = `${this.BASE_URL}/group/` + id;
+    let url: string = `${this.BASE_URL}/groups/` + id ;
+    return this.http.get(url, {headers: this.headers}).toPromise();
+  }
+
+  getGroups(): Promise<any> {
+    let url: string = `${this.BASE_URL}/groups`;
     return this.http.get(url, {headers: this.headers}).toPromise();
   }
 
   getGroupTransactions(id): Promise<any> {
-    let url: string = `${this.BASE_URL}/transactions/` + id;
+    let url: string = `${this.BASE_URL}/groups/` + id + '/transactions';
     return this.http.get(url, {headers: this.headers}).toPromise();
   }
 
   addGroup(group): Promise<any> {
-    let url: string = `${this.BASE_URL}/group`;
+    let url: string = `${this.BASE_URL}/groups`;
     return this.http.post(url, group, {headers: this.headers}).toPromise();
   }
 
   joinGroup(token): Promise<any> {
-    let url: string = `${this.BASE_URL}/group/` + token;
+    let url: string = `${this.BASE_URL}/groups/` + token;
     return this.http.get(url, {headers: this.headers}).toPromise();
   }
 
-  addExpense(expense): Promise<any> {
-    let url: string = `${this.BASE_URL}/expense`;
+  addExpense(expense, group_id): Promise<any> {
+    console.log(expense);
+    let url: string = `${this.BASE_URL}/groups/` + group_id + '/transactions';
     return this.http.post(url, expense, {headers: this.headers}).toPromise();
   }
 }
